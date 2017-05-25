fd66bcfd55d8f72adc409e78b12b2eb5b7499528
Mich Iline <eneismijmich@gmail.com>
2017-05-25 22:33:56 +0200

    working version

--- src/NodeRank.java
+++ src/NodeRank.java
@@ -1,58 +1,16 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.io.*;
+import java.util.*;
 
 /**
  * Created by michiline on 24/05/2017.
  */
 public class NodeRank {
 
-    public static class Matrix {
-
-        // return n-by-n identity matrix I
-        public static double[][] identity(int n) {
-            double[][] a = new double[n][n];
-            for (int i = 0; i < n; i++)
-                a[i][i] = 1;
-            return a;
-        }
-
-        // matrix-vector multiplication (y = A * x)
-        public static double[] multiply(double[][] a, double[] x) {
-            int m = a.length;
-            int n = a[0].length;
-            if (x.length != n) throw new RuntimeException("Illegal matrix dimensions.");
-            double[] y = new double[m];
-            for (int i = 0; i < m; i++)
-                for (int j = 0; j < n; j++)
-                    y[i] += a[i][j] * x[j];
-            return y;
-        }
-
-    }
-
-    public static class Node {
-        // value
-        double r;
-        // out degree
-        int d;
-
-        public Node (int r, int d) {
-            this.r = r;
-            this.d = d;
-        }
-    }
-
+    static final int ITERATIONS = 100;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("lab4A_primjeri/btest2/R.in"));
-
+        //BufferedReader br = new BufferedReader(new FileReader("examples/ttest2/R.in"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
         String[] split = line.split(" ");
 
@@ -60,34 +18,57 @@ public class NodeRank {
         int n = Integer.parseInt(split[0]);
         // beta
         double b = Double.parseDouble(split[1]);
+        // key = iteration; value = rank vector
+        Map<Integer, double[]> r = new HashMap<>();
+        // key = node; value = nodes that go into this node
+        Map<Integer, List<Integer>> inputs = new HashMap<>();
+        // degrees
+        int[] d = new int[n];
+
         // init r(t) vector
-        double[] r = new double[n];
+        double[] rt = new double[n];
         for (int i = 0; i < n; i++) {
-            r[i] = 1.0 / n;
+            rt[i] = 1.0 / n;
         }
+        r.put(0, rt);
         // r(t+1)
-        double[] rn = Arrays.copyOf(r, r.length);
-        int len;
+        double[] rn;
+        // init inputs
+        for (int i = 0; i < n; i++) {
+            inputs.put(i, new ArrayList<>());
+        }
 
-
-        Map<Integer, int[]> m = new HashMap<>();
-
+        // each line contains node outputs
+        // need to calculate node inputs
         for (int i = 0; i < n; i++) {
             line = br.readLine();
-            int[] d = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
-            m.put(i,d);
-            len = split.length;
-            for (int j = 0; j < len; j++) {
-                System.out.println(b * r[i] / len);
-                rn[d[j]] += b * r[i] / len;
+            int[] outNodes = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+            d[i] = outNodes.length;
+            for (int outNode : outNodes) {
+                inputs.get(outNode).add(i);
             }
         }
-        r = Arrays.copyOf(rn, rn.length);
-        rn = new double[n];
-        for (int i = 0; i < n; i++) {
-            rn[i] = 1.0 / n;
+
+        for (int k = 0; k < ITERATIONS; k++) {
+            rt = r.get(k);
+            rn = new double[n];
+            for (int j = 0; j < n; j++) {
+                List<Integer> input = inputs.get(j);
+                double sum = 0;
+                for (int i : input) {
+                    sum += rt[i] / d[i];
+                }
+                rn[j] = b * sum + (1 - b) / n;
+            }
+            r.put(k + 1, rn);
         }
 
+        int q = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < q; i++) {
+            int[] values = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            System.out.printf("%.10f\n", r.get(values[1])[values[0]]);
+        }
         System.out.println();
     }
 }
